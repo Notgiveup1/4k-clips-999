ba74bb95d9ac8ebdb13ab6194e3be76007fe3014
Hyzen <thisisnotagiveup@gmail.com>
2025-06-19 23:03:43 +0530

    added schmas.py

---
+++ schmas.py
@@ -0,0 +1,58 @@
+﻿from pydantic import BaseModel
+from typing import Optional, List
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class AnimeBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class AnimeCreate(AnimeBase):
+    pass
+
+class Anime(AnimeBase):
+    id: int
+    preview_image: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+class ClipBase(BaseModel):
+    title: str
+    anime_id: int
+
+class ClipCreate(ClipBase):
+    pass
+
+class Clip(ClipBase):
+    id: int
+    filename: str
+    preview_url: str
+
+    class Config:
+        from_attributes = True
+
+class VoteAnimeBase(BaseModel):
+    name: str
+    submitted_by: Optional[str] = None
+
+class VoteAnimeCreate(VoteAnimeBase):
+    pass
+
+class VoteAnime(VoteAnimeBase):
+    id: int
+    votes: int
+
+    class Config:
+        from_attributes = True
+
+class User(BaseModel):
+    username: str
+
+class UserInDB(User):
+    password: str
